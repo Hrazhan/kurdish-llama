56cb4021a323aa4fc6c869af609da66b5ae9c8d9
Razhan Hameed <razhan13@gmail.com>
2023-04-14 22:37:01 +0300

    add fine-tuning script

---
+++ finetune.py
@@ -0,0 +1,191 @@
+import os
+import torch
+import argparse
+import torch.nn as nn
+from datasets import load_dataset
+import transformers
+from transformers import LlamaForCausalLM, LlamaTokenizer
+from peft import (
+    prepare_model_for_int8_training,
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--micro_batch_size', type=int, default=32, help='micor batch size')
+parser.add_argument('--epochs', type=int, default=3, help='epochs')
+
+args = parser.parse_args()
+
+
+# Parameters
+MICRO_BATCH_SIZE = args.micro_batch_size
+BATCH_SIZE = 64
+GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
+EPOCHS = args.epochs
+LEARNING_RATE = 3e-4
+CUTOFF_LEN = 512
+LORA_R = 8
+LORA_ALPHA = 16
+LORA_DROPOUT = 0.05
+VAL_SET_SIZE = 2000
+TARGET_MODULES = [
+    "q_proj",
+    "k_proj",
+    "v_proj",
+    "down_proj",
+    "gate_proj",
+    "up_proj",
+]
+DATA_PATH = "data/alpaca_gpt4_ckb.json"
+OUTPUT_DIR = "checkpoints/"
+
+device_map = "auto"
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+ddp = world_size != 1
+if ddp:
+    device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+    GRADIENT_ACCUMULATION_STEPS = GRADIENT_ACCUMULATION_STEPS // world_size
+
+model = LlamaForCausalLM.from_pretrained(
+    "decapoda-research/llama-7b-hf",
+    load_in_8bit=True,
+    device_map=device_map,
+)
+total_params, params = 0, 0
+
+tokenizer = LlamaTokenizer.from_pretrained(
+    "decapoda-research/llama-7b-hf", add_eos_token=True
+)
+
+model = prepare_model_for_int8_training(model)
+
+config = LoraConfig(
+    r=LORA_R,
+    lora_alpha=LORA_ALPHA,
+    target_modules=TARGET_MODULES,
+    lora_dropout=LORA_DROPOUT,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+config.save_pretrained(OUTPUT_DIR)
+
+model = get_peft_model(model, config)
+tokenizer.pad_token_id = 0
+
+data = load_dataset("json", data_files=DATA_PATH)
+
+
+
+
+for n, p in model.model.named_parameters():
+    if any([x in n for x in ["lora"]]):
+        total_params += p.numel()
+    params += p.numel()
+
+print(
+    "Total number of parameters: {}M, rate: {}%".format(
+        total_params // 1000 / 1000, round(total_params / params * 100, 2)
+    )
+)
+
+
+
+def generate_and_tokenize_prompt(data_point):
+    # This function masks out the labels for the input,
+    # so that our loss is computed only on the response.
+    user_prompt = (
+        (
+            f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+            ### Instruction:
+            {data_point["instruction"]}
+            ### Input:
+            {data_point["input"]}
+            ### Response:
+            """
+        )
+        if data_point["input"] else 
+        (
+            f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+            ### Instruction:
+            {data_point["instruction"]}
+            ### Response:
+            """
+        )
+    )
+    len_user_prompt_tokens = (
+        len(
+            tokenizer(
+                user_prompt,
+                truncation=True,
+                max_length=CUTOFF_LEN + 1,
+            )["input_ids"]
+        )
+        - 1
+    )  # no eos token
+    full_tokens = tokenizer(
+        user_prompt + data_point["output"],
+        truncation=True,
+        max_length=CUTOFF_LEN + 1,
+        padding="max_length",
+    )["input_ids"][:-1]
+    return {
+        "input_ids": full_tokens,
+        "labels": [-100] * len_user_prompt_tokens
+        + full_tokens[len_user_prompt_tokens:],
+        "attention_mask": [1] * (len(full_tokens)),
+    }
+
+
+if VAL_SET_SIZE > 0:
+    train_val = data["train"].train_test_split(
+        test_size=VAL_SET_SIZE, shuffle=True, seed=42
+    )
+    train_data = train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+    val_data = train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+else:
+    train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
+    val_data = None
+
+
+
+# Training
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=val_data,
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=MICRO_BATCH_SIZE,
+        per_device_eval_batch_size=MICRO_BATCH_SIZE,
+        gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+        warmup_steps=100,
+        num_train_epochs=EPOCHS,
+        learning_rate=LEARNING_RATE,
+        fp16=True,
+        logging_steps=20,
+        evaluation_strategy="steps" if VAL_SET_SIZE > 0 else "no",
+        save_strategy="steps",
+        eval_steps=200 if VAL_SET_SIZE > 0 else None,
+        save_steps=200,
+        output_dir=OUTPUT_DIR,
+        save_total_limit=100,
+        load_best_model_at_end=True if VAL_SET_SIZE > 0 else False,
+        ddp_find_unused_parameters=False if ddp else None,
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+model.config.use_cache = False
+
+old_state_dict = model.state_dict
+model.state_dict = (
+    lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+).__get__(model, type(model))
+
+if torch.__version__ >= "2" and sys.platform != "win32":
+    model = torch.compile(model)
+
+
+trainer.train()
+
+model.save_pretrained(OUTPUT_DIR)
